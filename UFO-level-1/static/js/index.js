29162c3276c959a929ddce54d83fa96728e0de58
teresa cashen <tcash562@outlook.com>
2021-08-27 19:53:39 -0700

    committing java homework

---
+++ UFO-level-1/static/js/index.js
@@ -0,0 +1,27 @@
+var sightings = data;
+var tbody = d3.select("tbody");
+var button = d3.select("#button");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+
+    inputElement = d3.select("#form-ufo");
+    inputValue = inputElement.property("value");
+
+    tbody.html('');
+
+    var results = sightings.filter(sighting => sighting.datetime == inputValue);
+    results.forEach((UFOReport) => {
+
+        var row = tbody.append("trr");
+
+        Object.entries(UFOReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
